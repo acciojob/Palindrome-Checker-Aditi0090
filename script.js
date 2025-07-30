664e36b8de140baa9cceaac278b4069eef712a6a
Aditi0090 <165465425+Aditi0090@users.noreply.github.com>
2025-07-30 17:32:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// complete the given function
-
 function palindrome(str){
-
+    let cleaned = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    return cleaned === cleaned.split('').reverse().join('');
 }
-module.exports = palindrome
+
+module.exports = palindrome;
